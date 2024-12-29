6f90e9779621224bd7e2533b244024da4156d445
Oleg Erofeev <125142060+Edinorozka@users.noreply.github.com>
2024-12-29 13:35:47 +0300

    All files

--- bookland-backend/src/main/java/com/booklnad/bookland/DB/repository/ArticlesRepository.java
+++ bookland-backend/src/main/java/com/booklnad/bookland/DB/repository/ArticlesRepository.java
@@ -1,7 +1,6 @@
 package com.booklnad.bookland.DB.repository;
 
 import com.booklnad.bookland.DB.entity.Articles;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
